8f6aa334890851bb385ca3eb3d024a6215fd25bb
Nathan Ferreira <14365254+nathan130200@users.noreply.github.com>
2024-04-10 17:12:14 -0300

    Removed extended docstring in xmpp member attribute.

--- XmppSharp/Attributes/XmppMemberAttribute.cs
+++ XmppSharp/Attributes/XmppMemberAttribute.cs
@@ -1,25 +1,6 @@
 ﻿namespace XmppSharp.Attributes;
-
-using XmppSharp.Protocol.Tls;
-
 /// <summary>
 /// Annotating a field or property with this attribute helps when retrieving metadata associated with it.
-/// <para>
-/// Example declaring an XMPP enum (using the enum <see cref="StartTlsPolicy"/> as a reference):
-/// <code>
-/// <![CDATA[
-/// [XmppEnum]
-/// public enum StartTlsPolicy
-/// {
-///     [XmppMember("optional")]
-///     Optional,
-///     
-///     [XmppMember("required")]
-///     Required
-/// }
-/// ]]>
-/// </code>
-/// </para>
 /// </summary>
 [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
 public sealed class XmppMemberAttribute : Attribute
